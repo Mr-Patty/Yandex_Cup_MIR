7e1bc7783a727eb8d9e4ae8e6826f112db8e86c0
Shevchenko Oleg <shevchenko.cmc@gmail.com>
2024-10-16 22:51:38 +0300

    added colab config

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from models.train_module import TrainModule
 from utils import initialize_logging, load_config
 
-config = load_config(config_path="./config/config.yaml")
-initialize_logging(config_path="./config/logging_config.yaml", debug=False)
+import argparse
+
+
+# Создание парсера
+parser = argparse.ArgumentParser(description="Пример программы с argparse")
+
+# Добавление аргументов
+parser.add_argument('--config', type=str, help='Возраст пользователя', default="./config/config_colab.yaml")
+parser.add_argument('--log_config', type=str, help='Возраст пользователя', default="./config/logging_config.yaml")
+
+args = parser.parse_args()
+
+config = args.config
+log_conf = args.log_config
+
+config = load_config(config_path=config)
+initialize_logging(config_path=log_conf, debug=False)
 trainer = TrainModule(config)
 trainer.pipeline()
 trainer.test()
